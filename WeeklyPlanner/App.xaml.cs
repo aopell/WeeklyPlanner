72b0f4a6d4e36312b29e65e80e05757f1e98f42e
Aaron Opell <aaron.opell@live.com>
2020-01-21 18:26:21 -0800

    Fixed incorrect bindings

--- WeeklyPlanner/App.xaml.cs
+++ WeeklyPlanner/App.xaml.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
+using System.Windows.Data;
 
 namespace WeeklyPlanner
 {
@@ -13,5 +14,28 @@ namespace WeeklyPlanner
     /// </summary>
     public partial class App : Application
     {
+    }
+
+    [ValueConversion(typeof(bool), typeof(bool))]
+    public class InverseBooleanConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter,
+                              System.Globalization.CultureInfo culture)
+        {
+            if (targetType != typeof(bool))
+                throw new InvalidOperationException("The target must be a boolean");
+
+            return !(bool)value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter,
+                                  System.Globalization.CultureInfo culture)
+        {
+            throw new NotSupportedException();
+        }
+
+        #endregion
     }
 }
